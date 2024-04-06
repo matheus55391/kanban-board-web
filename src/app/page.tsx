6c0f0154f6eda967b6fc4e67b0af2513e10f2157
matheus55391 <matheus.manulo55@gmail.com>
2024-04-06 02:37:30 -0300

    feat: test ci.yaml

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export const metadata = {
 export default function Page() {
   return (
     <div className="flex justify-center items-center h-screen ">
+      
       <h1 className="text-4xl font-bold" data-testid="title">Kanban Board</h1>
     </div>
   )
