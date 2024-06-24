63eca33142812b96efddddb2498b62f614826de8
matheus55391 <matheus.felipe55391@gmail.com>
2024-06-24 14:17:33 -0300

    Base homepage

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,47 @@
+import KanbanBoard from "@/components/KanbanBoard";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { IoSettings, IoLogoGithub, IoCube, IoPeople, IoLogoBuffer } from "react-icons/io5";
+
+const Page: React.FC = () => {
+  return (
+    <div className="flex h-screen bg-gray-100">
+      <div className="bg-[#261C4D] w-14 flex flex-col items-center justify-between p-2 ">
+        <div className="h-10 w-10 flex items-center justify-center  rounded-full overflow-hidden border-2 ">
+          <Image
+            src="https://avatars.githubusercontent.com/u/35656197?s=400&u=9aa14daa4ab5efc5df2a013aaf518ff77e963098&v=4"
+            alt="Logo"
+            width={40}
+            height={40}
+          />
+        </div>
+        <div className="flex flex-col items-center space-y-6">
+          <IoCube className="text-white" size={24} />
+          <IoPeople className="text-white" size={24} />
+          <Link
+            href="https://github.com/matheus55391/kanban_board"
+            target="_blank"
+            className="text-white"
+          >
+            <IoLogoGithub className="text-white" size={24} />
+          </Link>
+        </div>
+        <div className="py-2">
+          <IoSettings className="text-white" size={24} />
+        </div>
+      </div>
+      <div className="flex-1 max-h-screen overflow-scroll">
+        <div className="flex flex-row items-center pl-4">
+          <IoLogoBuffer className="text-black" size={24} />
+          <h1 className="text-2xl font-bold text-center p-3">
+            Project Board
+          </h1>
+        </div>
+        <KanbanBoard />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,47 @@
-import KanbanBoard from "@/components/KanbanBoard";
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { IoSettings, IoLogoGithub, IoCube, IoPeople, IoLogoBuffer } from "react-icons/io5";
+import { BiRightIndent } from "react-icons/bi";
+import { FaArrowRight } from "react-icons/fa";
 
 const Page: React.FC = () => {
   return (
-    <div className="flex h-screen bg-gray-100">
-      <div className="bg-[#261C4D] w-14 flex flex-col items-center justify-between p-2 ">
-        <div className="h-10 w-10 flex items-center justify-center  rounded-full overflow-hidden border-2 ">
-          <Image
-            src="https://avatars.githubusercontent.com/u/35656197?s=400&u=9aa14daa4ab5efc5df2a013aaf518ff77e963098&v=4"
-            alt="Logo"
-            width={40}
-            height={40}
-          />
+    <div className="flex flex-col h-screen bg-zinc-50">
+      <header className=" text-black p-4 md:px-14 flex justify-between items-center">
+        <h1 className="text-2xl font-bold">KanbanBoard</h1>
+        <div className="flex gap-4">
+          <div className="text-black border  rounded-xl p-1 px-1  flex flex-row space-x-1 text-sm ">
+            <button className="hover:bg-gray-200 p-2 rounded-xl px-2 transition-all text-sm font-medium">
+              Log in
+            </button>
+            <button className="font-semibold text-white rounded-xl bg-rose-500 transition-all hover:bg-rose-600 font-medium text-sm px-2">
+              Sign Up
+            </button>
+          </div>
         </div>
-        <div className="flex flex-col items-center space-y-6">
-          <IoCube className="text-white" size={24} />
-          <IoPeople className="text-white" size={24} />
-          <Link
-            href="https://github.com/matheus55391/kanban_board"
-            target="_blank"
-            className="text-white"
-          >
-            <IoLogoGithub className="text-white" size={24} />
+      </header>
+
+      <main className="flex-grow flex flex-col  items-center text-center justify-center">
+        <h1 className="text-4xl font-bold mb-4 text-zinc-700">
+          The best way to organize your work
+        </h1>
+        <p className="mb-8">
+          Simplifying your job in a stylish and straightforward manner
+        </p>
+        <div>
+          <Link href="/dashboard">
+            <button className="flex flex-row  items-center  space-x-4 bg-rose-500 transition-all hover:bg-rose-600 text-white font-bold py-3 px-6 hover:py-4 hover:px-8  rounded">
+              <span>Get started. 100% FREE</span>
+              <FaArrowRight size={16} />
+            </button>
           </Link>
+          <p className="text-sm p-2 font-medium text-zinc-700">Free Forever. No Credit Card.</p>
         </div>
-        <div className="py-2">
-          <IoSettings className="text-white" size={24} />
-        </div>
-      </div>
-      <div className="flex-1 max-h-screen overflow-scroll">
-        <div className="flex flex-row items-center pl-4">
-          <IoLogoBuffer className="text-black" size={24} />
-          <h1 className="text-2xl font-bold text-center p-3">
-            Project Board
-          </h1>
-        </div>
-        <KanbanBoard />
-      </div>
+      </main>
+
+      <footer className="bg-zinc-100 p-6 text-center flex flex-row space-x-2 w-screen  ">
+        <p className="text-sm mr-auto">© 2024 KanbanBoard. All rights reserved.</p>
+        <p className="text-sm ">Build with ♥</p>
+      </footer>
     </div>
   );
 };
