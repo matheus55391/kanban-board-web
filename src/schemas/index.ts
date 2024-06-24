93c98aaff841ba3c05fcba497a15e9ef654130db
matheus55391 <matheus.felipe55391@gmail.com>
2024-06-24 18:13:42 -0300

    quantidade minima de strings da senha

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,5 +2,5 @@ import * as z from "zod"
 
 export const LoginSchema = z.object({
     email: z.string().email(),
-    password: z.string().min(10),
+    password: z.string().min(6, { message: "Password must be at least 6 characters long" }),
 })
