4b3988ccbd73fb69080a2ea4f73223ffda733c25
matheus55391 <matheus.felipe55391@gmail.com>
2024-08-15 18:50:18 -0300

    allow github image

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,10 @@ const nextConfig = {
             },
             {
                 protocol: 'https',
+                hostname: 'github.com',
+            },
+            {
+                protocol: 'https',
                 hostname: 'lh3.googleusercontent.com',
             }
         ]
