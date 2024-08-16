e4f1e40598b17546e81cd4dbc71c2ff565ba1ddf
matheus55391 <matheus.felipe55391@gmail.com>
2024-08-15 22:08:03 -0300

    guard for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { GuestGuard } from "@/components/guards/guest-guard";
 import StartPageLayout from "@/components/pages/start/StartPageLayout";
 import React from "react";
 
 const StartPage: React.FC = () => {
 
-  return <StartPageLayout />
+  return <GuestGuard><StartPageLayout /></GuestGuard>
 };
 
 export default StartPage;
