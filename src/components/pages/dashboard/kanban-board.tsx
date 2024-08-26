92aa28e668dff2a6900c2208fd6e4380cbb79b34
matheus55391 <matheus.manulo55@gmail.com>
2024-08-26 09:35:24 -0300

    feat/row dos filtros criada

--- src/components/pages/dashboard/kanban-board.tsx
+++ src/components/pages/dashboard/kanban-board.tsx
@@ -1,12 +1,17 @@
 "use client";
-import React, { useState } from "react";
-import { DragDropContext, DropResult } from "@hello-pangea/dnd";
-import ColumnComponent from "./column-component";
-import { BoardManager, State } from "@/utils/BoardManager";
-import { IoIosAdd, IoLogoBuffer } from "react-icons/io";
-import { MdOutlineModeEdit } from "react-icons/md";
+
 import useSession from "@/hooks/use-session";
+import { BoardManager, State } from "@/utils/BoardManager";
+import { DragDropContext, DropResult } from "@hello-pangea/dnd";
 import Image from "next/image";
+import { useState } from "react";
+import { CiFilter } from "react-icons/ci";
+import { IoIosAdd } from "react-icons/io";
+import { MdOutlineModeEdit } from "react-icons/md";
+import { RiGroupLine } from "react-icons/ri";
+import { SlGrid, SlLayers } from "react-icons/sl";
+import { TfiAngleDown } from "react-icons/tfi";
+import ColumnComponent from "./column-component";
 
 export default function KanbanBoard() {
   const [state, setState] = useState<State>(BoardManager.initialState);
@@ -34,28 +39,50 @@ export default function KanbanBoard() {
 
   return (
     <div className="flex flex-col w-full overflow-scroll">
-      <div className="flex flex-row items-center justify-between pl-4 ">
-        <div className="flex flex-row items-center space-x-2">
-          <h1 className="text-2xl font-bold text-center">Project Board</h1>
-          <div className="bg-gray-300 rounded-md h-5 w-5 flex items-center justify-center p-1 ">
-            <MdOutlineModeEdit size={12} color="black" />
+      <div className="flex flex-col w-full px-4 space-y-12 py-8">
+        <div className="flex flex-row items-center justify-between ">
+          <div className="flex flex-row items-center space-x-4">
+            <h1 className="text-4xl font-bold text-center">Project Board</h1>
+            <div className="bg-gray-200 rounded-md h-5 w-5 flex items-center justify-center p-1 ">
+              <MdOutlineModeEdit size={14} color="black" />
+            </div>
+          </div>
+          <div className="flex flex-row items-center space-x-2">
+            <div className="flex flex-row items-center space-x-2">
+              <div className="bg-gray-200 rounded-md h-5 w-5 flex items-center justify-center p-1 ">
+                <IoIosAdd size={16} color="black" />
+              </div>
+              <span className="text-sm font-medium text-gray-500">Invite</span>
+            </div>
+            <div className="flex flex-row">
+              <Image
+                src={user?.avatar || "/avatar.png"}
+                width={32}
+                height={32}
+                alt="user profile"
+                className="rounded-full"
+              />
+            </div>
           </div>
         </div>
-        <div className="flex flex-row items-center space-x-2">
-          <div className="flex flex-row items-center space-x-2">
-            <div className="bg-gray-300 rounded-md h-5 w-5 flex items-center justify-center p-1 ">
-              <IoIosAdd size={16} color="black" />
-            </div>
-            <span className="text-sm font-medium text-gray-500">Invite</span>
+        <div className="flex flex-row items-center justify-between ">
+          <div className="flex flex-row space-x-2 items-center justify-center border-2 p-2 border-gray-300 rounded-md max-w-28 px-3">
+            <CiFilter size={18} color="gray" />
+            <span className="text-sm font-normal text-gray-500">Filter</span>
+            <TfiAngleDown size={18} color="gray" />
           </div>
-          <div className="flex flex-row">
-            <Image
-              src={user?.avatar || "/avatar.png"}
-              width={32}
-              height={32}
-              alt="user profile"
-              className="rounded-full"
-              />
+          <div className="flex flex-row h-full space-x-2 items-center ">
+            <div className="flex flex-row space-x-2 items-center justify-center border-2 p-2 border-gray-300 rounded-md max-w-28 px-3">
+              <RiGroupLine size={18} color="gray" />
+              <span className="text-sm font-normal text-gray-500">Share</span>
+            </div>
+            <div className="h-8 w-[1px] bg-gray-400 flex" />
+            <div className="h-10 w-10 bg-gray-200 rounded-lg p-1 flex items-center justify-center hover:cursor-pointer">
+              <SlGrid size={20} color="gray" />
+            </div>
+            <div className="h-10 w-10 bg-gray-50 rounded-lg p-1 flex items-center justify-center hover:cursor-pointer">
+              <SlLayers size={20} color="gray" />
+            </div>
           </div>
         </div>
       </div>
