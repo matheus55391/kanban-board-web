78accc3ce0448e604f61bc2b167a4c8988ce00d2
matheus55391 <matheus.felipe55391@gmail.com>
2024-08-31 23:16:04 -0300

    feat/criada nova homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+import { HomePageView } from "@/components/home/home-page-view";
 import { GuestGuard } from "@/components/guards/guest-guard";
-import WelcomeView from "@/components/home/welcome-view";
 import React from "react";
 
 const StartPage: React.FC = () => {
   return (
     <GuestGuard>
-      <WelcomeView />
+      <HomePageView />
     </GuestGuard>
   );
 };
